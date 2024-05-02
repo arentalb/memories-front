1a5e33e0c426c441d79c20292075d05b626196f2
aren talb <aren.talb00@gmail.com>
2024-05-02 11:32:09 +0300

    Design main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-
+import { Posts } from "./components/Posts/Posts.tsx";
+import { Form } from "./components/Form/Form.tsx";
 
 function App() {
-
   return (
     <>
-      hello world
+      <div
+        className={
+          "bg-blue-300  flex justify-center items-center gap-10 rounded-md m-4 p-4"
+        }
+      >
+        <h1 className={"text-3xl font-bold"}>Memories</h1>
+        <img src={"img.png"} alt={"memories"} className={"w-20"} />
+      </div>
+
+      <div className="container mx-auto p-4">
+        <div className="flex flex-wrap -mx-3">
+          <div className="w-full sm:w-7/12 p-3">
+            <Posts />
+          </div>
+          <div className="w-full sm:w-4/12 p-3">
+            <Form />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,11 @@
+import { Post } from "./Post.tsx";
+
+export function Posts() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
