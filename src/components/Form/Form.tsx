69ca4d4a46e4bbcdfc1222190df243791c8e9c5b
aren talb <aren.talb00@gmail.com>
2024-05-02 12:59:56 +0300

    Design the form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,154 @@
+import { ChangeEvent, useRef, useState } from "react";
+
 export function Form() {
-  return <div>Form</div>;
+  const [formValue, setFormValue] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+  });
+  const [imageFile, setImageFile] = useState<File | null>(null);
+
+  function changeHandler(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormValue((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <div className={"flex flex-col gap-6 w-full"}>
+      <Input
+        onChange={changeHandler}
+        name={"creator"}
+        placeholder={"Creator"}
+        value={formValue.creator}
+      />
+      <Input
+        onChange={changeHandler}
+        name={"title"}
+        placeholder={"Title"}
+        value={formValue.title}
+      />
+      <Input
+        onChange={changeHandler}
+        name={"message"}
+        placeholder={"Message"}
+        value={formValue.message}
+      />
+
+      <Input
+        onChange={changeHandler}
+        name={"tags"}
+        placeholder={"Tags"}
+        value={formValue.tags}
+      />
+      <FileInput imageFile={imageFile} setImageFile={setImageFile} />
+      <div className={"grid w-full grid-cols-2 gap-3"}>
+        <Button
+          onClick={() => {}}
+          text={"Save"}
+          className={"bg-blue-200 hover:bg-blue-300"}
+        />
+        <Button
+          onClick={() => {}}
+          text={"Clear"}
+          className={"bg-red-200 hover:bg-red-300"}
+        />
+      </div>
+    </div>
+  );
+}
+
+interface ButtonProps {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  text: string;
+  className: string;
+}
+
+function Button({ text, className, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`h-12 rounded-md font-semibold text-lg text-gray-900 transition-all duration-200 ease-in-out  ${className}`}
+    >
+      {text}
+    </button>
+  );
+}
+
+interface InputProps {
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  placeholder: string;
+  value: string;
+}
+
+function Input({ onChange, name, placeholder, value }: InputProps) {
+  return (
+    <div>
+      <input
+        type="text"
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        className={
+          "px-4 py-3 border-2 w-full rounded-md text-lg  placeholder-gray-600"
+        }
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+interface FileInputProps {
+  accept?: string;
+  imageFile: File | null;
+  setImageFile: (file: File | null) => void;
+}
+
+export function FileInput({
+  accept = "image/*",
+  imageFile,
+  setImageFile,
+}: FileInputProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUploadClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const updateImage = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      setImageFile(file);
+    }
+  };
+
+  return (
+    <div className="px-4 py-3 border-2 rounded-md text-lg  placeholder-gray-600 ">
+      <div
+        className=" p-2  border-2 border-dashed border-gray-300 text-gray-400 text-lg rounded-md  cursor-pointer  "
+        onClick={handleImageUploadClick}
+      >
+        {imageFile ? (
+          <img
+            src={URL.createObjectURL(imageFile)}
+            alt="Uploaded"
+            className="  rounded-md object-cover self-start"
+          />
+        ) : (
+          <span>Choose file or drag it here</span>
+        )}
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept={accept}
+          className=" hidden"
+          onChange={updateImage}
+        />
+      </div>
+    </div>
+  );
 }
