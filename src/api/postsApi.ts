12fd91579a835aef7d72857c30a9e62896152dd9
aren talb <aren.talb00@gmail.com>
2024-05-03 15:08:53 +0300

    fix some bugs in api

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -3,6 +3,7 @@ import { PostBase } from "../types/TPost.ts";
 
 const url = "http://localhost:6060/posts";
 
+//https://axios-http.com/docs/res_schema
 async function fetchPosts() {
   const response = await axios.get(url);
   return response.data.data;
@@ -10,14 +11,14 @@ async function fetchPosts() {
 
 async function createPost(newPost: PostBase) {
   const response = await axios.post(url, newPost);
-  return response.data;
+  return response.data.data;
 }
 
 async function updatePost(obj: { selectedId: string; newPost: PostBase }) {
   const { selectedId, newPost } = obj;
 
   const response = await axios.patch(`${url}/${selectedId}`, newPost);
-  return response.data;
+  return response.data.data;
 }
 
 async function fetchPostById(id: string) {
